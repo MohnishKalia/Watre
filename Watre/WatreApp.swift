98555dd4dde50dbec617f36f83de2ed4eeb2afe5
Mohnish Kalia <mohnish.kalia@gmail.com>
2022-12-30 20:56:47 -0600

    Initial Commit

---
+++ Watre/WatreApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatreApp.swift
+//  Watre
+//
+//  Created by Mohnish Kalia on 12/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WatreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
